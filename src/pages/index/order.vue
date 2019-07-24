8f24c76e874486cc7b7306f3fb6cd1439a5197d3
www <811585541@qq.com>
2019-07-24 21:44:35 +0800

    是

--- src/pages/index/order.vue
+++ src/pages/index/order.vue
@@ -5,58 +5,57 @@
         <div :class="'eachPeople bgW '+item.borderColor" v-for="item,index of postList" @click="toSeeOrderDetail(index)">
             <div class="titleBox flexSpace padding11">
                 <div class="left flexStart">
-                    <img :src="item.webUser?$store.state.imgUrl+item.webUser.imgurl:''" class="userImg" alt>
+                    <img :src="item.user?$store.state.imgUrl+item.user.imgurl:''" class="userImg" alt>
                     <div>
-                        <p class="name">{{item.webUser?item.webUser.nickname:''}}</p>
-                        <p class="options flexStart">
-                            <span class="option flexStart">
-                  实名
-                  <i class="iconfont icon-wenhao" v-if="item.webUser?item.webUser.realAuth!=2?true:false:''"></i>
-                  <i class="iconfont icon-gouxuan" v-if="item.webUser?item.webUser.realAuth!=2?false:true:''"></i>
-                </span>
-                            <span class="option">
-                  学历
-                  <i class="iconfont icon-wenhao" v-if="item.eduAuth!=2?true:false"></i>
-                  <i class="iconfont icon-gouxuan" v-if="item.eduAuth!=2?false:true"></i>
-                </span>
-                        </p>
+                        <p class="name">{{item.user?item.user.nickname:''}}</p>
                     </div>
                 </div>
                 <div class="right flexEnd">
-                    
-                     <p class="price">{{'￥'+item.priceMin+'-￥'+item.priceMax}}</p>
-                     
+                    <span class="priceDesc">费用预算</span>
+                    <p class="price">{{'￥'+item.fee.split('-')[0]+'-￥'+item.fee.split('-')[1]}}</p>
+
                 </div>
             </div>
-            <div class="details padding11">
-                <p class="hang flexSpace mainText">
-                    <span class="eachItem">状态：{{item.status}}</span>
-                    <span class="eachItem textAright">截止报名：{{item.deadline?item.deadline.substr(0,10):''}}</span>
-                </p>
-                <p class="hang flexSpace">
-                    <span
-              class="eachItem"
-            >{{item.classNo?item.classNo:''}}/{{item.category?item.category:''}}</span>
-                    <span class="eachItem textAright">已报名：{{item.applicationCount}}</span>
-                    <!-- <span class="eachItem">组班方式：{{item.taskType}}</span> -->
-                </p>
-                <p class="hang flexStart">
-                    <span class="eachItem">{{item.coordination}}/{{item.area}}</span>
-                    <span class="eachItem add textAright">发布日期：{{item.createTime?item.createTime.substr(0,10):''}}</span>
-                </p>
+            <div class="content">
+                <div class="details">
+                    <p class="hang flexStart">
+                        <span class="eachItem">年级：{{item.classNo}}</span>
+                        <span class="eachItem">科目：{{item.subject}}</span>
+                    </p>
+                    <p class="hang flexStart">
+                        <span class="eachItem">上课：{{item.coordination=='online'?'远程协作':item.coordination=='busniess'?'家长拜访':'家教拜访'}}</span>
+
+                        <span class="eachItem add">区域：{{item.area}}</span>
+                    </p>
+
+                    <p class="hang">
+                        <span>简述：{{item.introduction}}</span>
+                    </p>
+                </div>
             </div>
-            <div class="otherMsg flexSpace padding11">
-                <p class="hang">
-                    <span>简述：{{item.introduction}}</span>
+            <div class="otherMsg flexSpace">
+                <p class="left">
+                    <span>截至报名：{{!item.deadline?"":item.deadline.substr(0,10)}}</span>
+
+                    <span>已报名：{{item.applicationCount}}</span>
+                </p>
+
+                <p class="right">
+                    <!-- <button class="submitBtn" @click.stop="toEnroll(index)" v-if="$store.state.userType=='author'">报名</button> -->
+                    <button :class="$store.state.userType == 'author'&&item.status=='3'||$store.state.userType == 'business'&&item.status=='2'?'submitBtn':'submitBtn submitBtnDis'">
+                        {{item.statusText}}
+                    </button>
                 </p>
             </div>
         </div>
     </div>
     <!-- 任务列表 end-->
+    <appFooter :nowPath="nowPath"></appFooter>
 </div>
 </template>
 
 <script>
+import appFooter from "@/components/footer/appFooter";
 export default {
     data() {
         return {
@@ -66,8 +65,15 @@ export default {
             uid: this.$route.query.uid,
             isCancel: false,
             userData: {},
-            showAllMsg: false
+            showAllMsg: false,
+            nowPath: '/order'
         };
+    },
+    components: {
+        appFooter
+    },
+    created() {
+        this.getMIssionlist();
     },
     mounted() {
 
@@ -76,7 +82,7 @@ export default {
         if (this.$route.query.showAllMsg) {
             this.showAllMsg = this.$route.query.showAllMsg;
         }
-        this.getMIssionlist();
+
     },
     methods: {
         getUser(uid) {
@@ -99,17 +105,62 @@ export default {
             if (autherId == this.$store.state.uid) {
                 isAuthor = true;
             }
-           
+
         },
         // 获取任务列表
-        getMIssionlist(){
-            let that=this;
-            that.$http('get', that.$store.state.baseUrl + 'api/Order/List?uid=' + that.$store.state.uid).then(function (res) {
-                that.postList = res.data.data;
-            })
+        getMIssionlist() {
+            const that = this;
+            // 对家长状态(1-报名中,2-已选择,3-已评价,4-已失效,5-已取消),, 对家教状态(1-已报名,2-未选中,3-待评价,4-已完成)) ,
+            if (that.$store.state.userType == 'author') {
+                // 家教获取订单列表
+                that.$http('get', that.$store.state.baseUrl + 'api/Order/apply?uid=' + that.$store.state.uid).then(function (res) {
+                    res.data.data.forEach((item, index) => {
+                        switch (item.status) {
+                            case 1:
+                            item.statusText='已报名'
+                            break;
+                            case 2:
+                            item.statusText='未选中'
+                            break;
+                            case 3:
+                            item.statusText='待评价'
+                            break;
+                            case 4:
+                            item.statusText='已结束'
+                            break;
+                        }
+                    })
+                    that.postList = res.data.data;
+
+                })
+            } else {
+                // 家长获取发布的订单
+                that.$http('get', that.$store.state.baseUrl + 'api/Order/List?uid=' + that.$store.state.uid).then(function (res) {
+                    res.data.data.forEach((item, index) => {
+                        switch (item.status) {
+                            case 1:
+                            item.statusText='报名中'
+                            break;
+                            case 2:
+                            item.statusText='待评价'
+                            break;
+                            case 3:
+                            item.statusText='可追评'
+                            break;
+                            case 4:
+                            item.statusText='已失效'
+                            break;
+                            case 5:
+                            item.statusText='已失效'
+                            break;
+                        }
+                    })
+                    that.postList = res.data.data;
+                })
+            }
+
         }
-       
-        
+
     }
 };
 </script>
@@ -118,6 +169,12 @@ export default {
 #Order {
     background: #f5f5f5;
     padding-bottom: 60px;
+    padding-top: 10px;
+}
+
+.priceDesc {
+    color: #707070;
+    font-size: 12px;
 }
 
 #Order .topTab {
@@ -126,6 +183,14 @@ export default {
     padding-left: 20px;
     padding-right: 20px;
     margin-bottom: 10px;
+}
+
+#order #resultPeoples .userImg {
+    width: 40px;
+    height: 40px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 50%;
 }
 
 #Order .topTab .item {
@@ -231,7 +296,7 @@ export default {
     padding: 11px 11px 14px 11px;
     color: #999999;
     font-size: 12px;
-    background: #f5f5f5;
+    /* background: #f5f5f5; */
 }
 
 .padding11 {
@@ -247,17 +312,23 @@ export default {
     font-weight: bold;
     border: none;
 }
+.eachPeople .submitBtn.submitBtnDis{
+     background: #B2B2B2;
+}
+#resultPeoples.reacResult .content {
+    padding: 5px 17px;
+}
 
 #resultPeoples.reacResult .details {
     display: block;
     color: #707070;
-    background: #fff;
-    padding: 14px 17px;
+    /* background: #fff; */
+    /* padding: 14px 17px; */
 }
 
 #resultPeoples.reacResult .titleBox {
     margin-bottom: 0;
-    border-bottom: 1px solid #cecece;
+    /* border-bottom: 1px solid #cecece; */
 }
 
 #resultPeoples.reacResult .hang {
@@ -332,4 +403,31 @@ export default {
     /* max-width: 30%; */
     text-overflow: ellipsis;
 }
+
+#resultPeoples.reacResult .details {
+    display: block;
+    color: #707070;
+    /* background-clip: content-box; */
+}
+
+#resultPeoples.reacResult .hang {
+    width: 100%;
+    text-align: left;
+    margin-bottom: 7px;
+    /* padding: 5px 10px; */
+}
+
+#resultPeoples.reacResult span.eachItem {
+    width: 50%;
+    display: inline-block;
+    max-width: 50%;
+}
+
+.smalltitle {
+    color: #242424;
+    font-size: 14px;
+    padding: 10px 16px;
+}
+
+/* // ======================== */
 </style>
